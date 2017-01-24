643cf79ef8d83f07e8c17a1cc45f27058d4a2b9d
Teemu Patja <tp@iki.fi>
2017-01-24 23:20:32 +0200

    Fix uberjar path

--- src/commiteth_pipeline/steps.clj
+++ src/commiteth_pipeline/steps.clj
@@ -22,4 +22,4 @@
 (defn deploy-uberjar [args ctx]
   (shell/bash ctx
               (:cwd args)
-              "pwd; ls -l;sudo service commiteth stop && cp target/commiteth.jar /opt/commiteth/commiteth.jar && sudo service commiteth start"))
+              "sudo service commiteth stop && cp target/uberjar/commiteth.jar /opt/commiteth/commiteth.jar && sudo service commiteth start"))
