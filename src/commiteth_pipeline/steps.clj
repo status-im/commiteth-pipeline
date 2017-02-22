54e50ec1fcbd84ed4eed5d95ece94656a4de968d
Teemu Patja <tp@iki.fi>
2017-02-22 20:17:17 +0200

    sassc -> less

--- src/commiteth_pipeline/steps.clj
+++ src/commiteth_pipeline/steps.clj
@@ -19,7 +19,7 @@
   (git/clone ctx repo-uri repo-branch (:cwd args)))
 
 (defn build-uberjar [args ctx]
-  (shell/bash ctx (:cwd args) "lein sassc once && lein uberjar"))
+  (shell/bash ctx (:cwd args) "lein less once && lein uberjar"))
 
 (defn run-tests [args ctx]
   (shell/bash ctx (:cwd args) "echo no test running for now"))
